151df58c10350fa4c0bde09c9955d4420c7528b0
Sugak Andrey <sugak.andr3y@gmail.com>
2017-04-06 01:42:09 +0300

    Mutation reordering in ScalamuConfig.allMutations

--- scalamu-plugin/src/main/scala/org/scalamu/plugin/ScalamuConfig.scala
+++ scalamu-plugin/src/main/scala/org/scalamu/plugin/ScalamuConfig.scala
@@ -6,21 +6,42 @@ import org.scalamu.plugin.mutations.arithmetic._
 import org.scalamu.plugin.mutations.methodcalls._
 
 object ScalamuConfig {
+
+  /**
+   * The order of mutations is important, in some cases wrong order can lead to nested mutations
+    * (unreachable code) being generated.
+    * e.g. if [[ReplaceMathOperators]] is applied after [[InvertNegations]] in x * -2
+    * {{{
+    * if (mu2) {
+    *   x / (if (mu1) 2 else -2)
+    * } else {
+    *   x * (if (mu1) 2 else -2)
+    * }
+    * }}}
+    * instead of
+    * {{{
+    * if (mu1) {
+    *   x / -2
+    * } else {
+    *   x * (if (mu2) 2 else -2)
+    * }
+    * }}}
+   */
   val allMutations: Seq[Mutation] = Seq(
-    InvertNegations,
+    ReplaceCaseWithWildcard,
     ReplaceMathOperators,
+    ReplaceWithIdentityFunction,
+    InvertNegations,
     AlwaysExecuteConditionals,
     NeverExecuteConditionals,
-    ChangeConditionalBoundaries, 
+    ChangeConditionalBoundaries,
     NegateConditionals,
     RemoveUnitMethodCalls,
-    ReplaceWithIdentityFunction,
     ChangeRangeBoundary,
     ReplaceLogicalOperators,
     ReplaceWithNone,
-    ReplaceWithNil,
-    ReplaceCaseWithWildcard
+    ReplaceWithNil
   )
-  
+
   val mutationGuardPrefix: String = "org.scalamu.guards"
 }
