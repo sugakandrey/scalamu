08da67e57740aca1f61cfc37d70a64d2c74220aa
Sugak Andrey <sugak.andr3y@gmail.com>
2017-06-22 14:20:14 +0300

    Fix html float formatting

--- report/src/main/scala/org/scalamu/report/CoverageStats.scala
+++ report/src/main/scala/org/scalamu/report/CoverageStats.scala
@@ -1,5 +1,8 @@
 package org.scalamu.report
 
+import java.text.NumberFormat
+import java.util.Locale
+
 import org.scalamu.core.TestedMutant
 import org.scalamu.core.coverage.Statement
 
@@ -8,11 +11,18 @@ trait CoverageStats {
   def invokedStatements: Set[Statement]
   def mutants: Set[TestedMutant]
 
+  private val formatter = {
+    val fmt = NumberFormat.getInstance(Locale.ROOT)
+    fmt.setMinimumFractionDigits(1)
+    fmt.setMaximumFractionDigits(1)
+    fmt
+  }
+
   def coverage: String =
     if (statements.isEmpty) "100"
-    else (invokedStatements.size * 100.0 / statements.size).formatted("%.1f")
+    else formatter.format(invokedStatements.size * 100.0 / statements.size)
 
   def mutationCoverage: String =
     if (mutants.isEmpty) "100"
-    else (mutants.count(_.status.killed) * 100.0 / mutants.size).formatted("%.1f")
+    else formatter.format(mutants.count(_.status.killed) * 100.0 / mutants.size)
 }
