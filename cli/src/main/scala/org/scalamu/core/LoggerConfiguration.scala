1335b491bf3c2ffa36e26f623bce7dd471be2503
Sugak Andrey <sugak.andr3y@gmail.com>
2017-06-26 20:41:13 +0300

    Make logback patterns clearly show which JVM are they running on

---
+++ cli/src/main/scala/org/scalamu/core/LoggerConfiguration.scala
@@ -0,0 +1,25 @@
+package org.scalamu.core
+
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.classic.{PatternLayout, Logger => LogbackLogger}
+import ch.qos.logback.core.ConsoleAppender
+import org.slf4j.{LoggerFactory, Logger => SL4JLogger}
+
+object LoggerConfiguration {
+  def configurePatternForName(name: String): Unit = {
+    val logger = LoggerFactory.getLogger(SL4JLogger.ROOT_LOGGER_NAME).asInstanceOf[LogbackLogger]
+    logger.detachAndStopAllAppenders()
+
+    val pl = new PatternLayout
+    val ctx = logger.getLoggerContext
+    pl.setContext(ctx)
+    pl.setPattern(s"%d{HH:mm:ss.SSS} [$name - %thread{5}] %-5level %logger{10} - %msg%n")
+    pl.start()
+
+    val stdoutAppender = new ConsoleAppender[ILoggingEvent]
+    stdoutAppender.setContext(ctx)
+    stdoutAppender.setLayout(pl)
+    stdoutAppender.start()
+    logger.addAppender(stdoutAppender)
+  }
+}
