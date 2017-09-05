037790a3c997549e1dc49a9c67e99bb2bed4c6f2
Sugak Andrey <sugak.andr3y@gmail.com>
2017-09-05 18:47:22 +0300

    Fix faulty duration measuring for ScalaTest suites

--- cli/src/main/scala/org/scalamu/testapi/scalatest/ScalaTestConverters.scala
+++ cli/src/main/scala/org/scalamu/testapi/scalatest/ScalaTestConverters.scala
@@ -8,22 +8,22 @@ import org.scalatest.{Reporter, Status, Stopper}
 import scala.collection.mutable
 
 class ScalaTestConverters extends SuiteResultTypeConverter[Status] {
-  private val failures = mutable.Set.empty[TestFailure]
+  private val failures       = mutable.Set.empty[TestFailure]
   private var duration: Long = _
 
   private def failureMessage(e: TestFailed): String =
     s"Test ${e.testName} in suite ${e.suiteName} failed."
 
   private[scalatest] val reporter: Reporter = {
-    case e: TestFailed   => failures += TestFailure(failureMessage(e), e.throwable.map(_.getMessage))
-    case e: SuiteAborted => failures += TestFailure(e.message, e.throwable.map(_.getMessage))
-    case e: RunCompleted => e.duration.foreach(duration = _)
-    case _               =>
+    case e: TestFailed    => failures += TestFailure(failureMessage(e), e.throwable.map(_.getMessage))
+    case e: SuiteAborted  => failures += TestFailure(e.message, e.throwable.map(_.getMessage))
+    case e: TestSucceeded => e.duration.foreach(duration += _)
+    case _                =>
   }
 
   private[scalatest] val stopper: Stopper = new Stopper {
     override def stopRequested: Boolean = failures.nonEmpty
-    override def requestStop(): Unit = ()
+    override def requestStop(): Unit    = ()
   }
 
   override def fromResult(suite: ClassName)(result: Status): TestSuiteResult =
