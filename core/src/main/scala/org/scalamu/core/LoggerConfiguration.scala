58e5617a6b69c4f0598bc7a3813ef460dc3c87e3
Sugak Andrey <sugak.andr3y@gmail.com>
2017-11-13 19:31:51 +0300

    Fix time conversions in logging

--- core/src/main/scala/org/scalamu/core/LoggerConfiguration.scala
+++ core/src/main/scala/org/scalamu/core/LoggerConfiguration.scala
@@ -1,6 +1,6 @@
 package org.scalamu.core
 
-import java.time.Instant
+import java.time.{Instant, ZoneId}
 import java.time.format.DateTimeFormatter
 
 import scribe.formatter.FormatterBuilder
@@ -11,12 +11,17 @@ object LoggerConfiguration {
     val level = if (verbose) Level.Debug else Level.Info
 
     val formatter = FormatterBuilder()
-      .add(record => DateTimeFormatter.ofPattern("HH:mm:ss.SSS").format(Instant.ofEpochMilli(record.timestamp)))
+      .add(
+        record =>
+          DateTimeFormatter
+            .ofPattern("HH:mm:ss.SSS")
+            .format(Instant.ofEpochMilli(record.timestamp).atZone(ZoneId.systemDefault()).toLocalDateTime)
+      )
       .string(s" [$name - ").threadName.string("] ")
       .levelPaddedRight.string(" ")
       .positionAbbreviated
       .string(" - ").message.newLine
-    
+
     val handler = LogHandler(level, formatter)
 
     Logger.root.clearHandlers()
